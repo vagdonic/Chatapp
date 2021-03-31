84e6aabf8abb734a8083d5af796ab4b2cbd71b8e
Manav Vagdoda <manav@laptop.local>
2021-03-31 08:28:50 +0530

    frontend complete

---
+++ javascript/pass-show-hide.js
@@ -0,0 +1,14 @@
+const pwdField = document.querySelector(".form .field input[type = 'password']"),
+toggleBtn = document.querySelector(".form .field i");
+
+toggleBtn.onclick = () => {
+    if(pwdField.type == "password"){
+        pwdField.type = "text";
+        toggleBtn.classList.add("active");
+    }
+    else{
+        pwdField.type = "password";
+        toggleBtn.classList.remove("active");
+    }
+
+}
